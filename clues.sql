1d2ff00f310faf3867f3c2f2a1d4535b0c32ad55
Haroon <you@example.com>
2024-08-27 23:51:24 +0300

    Add SQL queries to track Carmen Sandiego's whereabouts

--- clues.sql
+++ clues.sql
@@ -1,45 +1,14 @@
-CREATE TABLE cities (
-    id integer NOT NULL,
-    name text NOT NULL,
-    countrycode character(3) NOT NULL,
-    district text NOT NULL,
-    population integer NOT NULL
-);
-
-CREATE TABLE countries (
-    code character(3) NOT NULL,
-    name text NOT NULL,
-    continent text NOT NULL,
-    region text NOT NULL,
-    surfacearea real NOT NULL,
-    indepyear smallint,
-    population integer NOT NULL,
-    lifeexpectancy real,
-    gnp numeric(10,2),
-    gnpold numeric(10,2),
-    localname text NOT NULL,
-    governmentform text NOT NULL,
-    headofstate text,
-    capital integer,
-    code2 character(2) NOT NULL
-);
-
-CREATE TABLE countrylanguages (
-    countrycode character(3) NOT NULL,
-    language text NOT NULL,
-    isofficial boolean NOT NULL,
-    percentage real NOT NULL
-);
 
 
 -- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been traveling through Southern Europe.
 -- She's most likely traveling someplace where she won't be noticed,
 -- so find the least populated country in Southern Europe, and we'll start looking for her there.
 
-SELECT name, population, region FROM countries
-WHERE region = 'Southern Europe'
+-- we can use * to select all columns
+SELECT code,name, population, region FROM countries
+WHERE region='Southern Europe'
 ORDER BY population ASC
-LIMIT 1;
+LIMIT 1; -- to select only the least populated country in Southern Europe
 
 
 -- Clue #2: Now that we're here, 
@@ -72,24 +41,23 @@ AND name ='Italy' OR name = 'San Marino' OR name = 'Holy See (Vatican City State
 Order BY name DESC;
 -- the output : San Marino so the city is San Marino
 --find onther country in the same city.
-SELECT name FROM cities
+SELECT * FROM cities
 WHERE  countrycode = 'SMR';  -- the output : Seravalle
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
-SELECT ci.name AS city_name, co.name AS country_name,continent 
+SELECT ci.name AS city_name,continent, ci.population, ci.district, ci.countrycode,co.name AS country_name, co.region
 FROM cities ci , countries co
-WHERE ci.countrycode = co.code AND ci.name LIKE 'San M%' AND continent = 'South America'; 
+WHERE ci.countrycode = co.code AND ci.name LIKE 'Serra%' AND continent = 'South America'; 
 
-
+-- the capital id is 211;
 
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
 -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
 -- follow right behind you!
 
-SELECT ci.name AS City_NAME,co.name AS Counter_Name ,co.capital FROM countries co , cities ci
-WHERE ci.countrycode = co.code AND co.name ='Argentina' AND co.capital = ci.id;  
+SELECT id,name,countrycode,district FROM cities WHERE id='211';
 
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock. Lucky for us, she's getting cocky. She left us a note (below), and I'm sure she thinks she's very clever, but if we can crack it, we can finally put her where she belongs – behind bars.
@@ -102,5 +70,5 @@ WHERE ci.countrycode = co.code AND co.name ='Argentina' AND co.capital = ci.id;
 --               So I'm off to add one to the population I find
 --               In a city of ninety-one thousand and now, eighty five.
 
-
+    SELECT * FROM cities WHERE population=91084;
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
